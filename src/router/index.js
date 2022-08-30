ffeb9ae61038a0705d7f063aee4ece565d779b79
Jonathan Chan <Jonathanchan197@gmail.com>
2022-08-30 14:46:26 +1000

    signup page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Flowers from '../views/Flowers.vue';
+import Register from '../views/Register.vue'
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/flowers',
       name: 'flowers',
       component: Flowers
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 });
