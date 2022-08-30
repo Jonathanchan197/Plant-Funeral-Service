17e05ce88da2da49e4cef2f27e038354da68e8a0
dkdam <dkdam@live.com>
2022-08-30 20:47:44 +1000

    current hompage 1/3 done

--- src/views/Flowers.vue
+++ src/views/Flowers.vue
@@ -1,24 +1,58 @@
 <template>
     <div>
+        <div>
+        <b-navbar toggleable="lg" type="light" variant="light">
+            <b-navbar-brand href="#">Plant life after death</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Link</b-nav-item>
+                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                </b-nav-form>
+
+                <b-nav-item-dropdown text="Lang" right>
+                <b-dropdown-item href="#">EN</b-dropdown-item>
+                <b-dropdown-item href="#">ES</b-dropdown-item>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">FA</b-dropdown-item>
+                </b-nav-item-dropdown>
+
+                <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template #button-content>
+                    <em>User</em>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        </div>
         <b-card title="Card Title" no-body>
-            <b-card-header header-tag="nav">
-            <b-nav card-header pills>
-                <b-nav-text>Plant life after death</b-nav-text>
-            </b-nav>
-            </b-card-header>
-            <b-card bg-variant="light" text-variant="black" class="text-center" style="height: 680px">
+
+            <section class="title" style="">
                 <div class="heading">
                 <b-card-text>Love.</b-card-text>
                 <b-card-text>Respect.</b-card-text>
                 <b-card-text>Remember.</b-card-text>
                 </div>
-            </b-card>
+            </section>
 
             <b-card-body class="text-center">
 
-                <b-card-text class="service">personallised service every step of the way</b-card-text>
+            <b-card-text class="service">personallised service every step of the way</b-card-text>
 
-            <b-card bg-variant="light" text-variant="black" class="text-center" style="height: 680px">
+            <section style="height: 680px">
                 <div class="care">
                 <b-card-text class="did">Did you grow a plant for years so that a neighbor would be jealous... and it died?</b-card-text>
                 <b-card-text>Don’t worry we will take care of you. </b-card-text>
@@ -27,11 +61,11 @@
                     <h2>Our Story</h2>
                     <p>Hello, is it me you're looking for?  I can see it in your eyes, I can see it in your smile, You're all I've ever wanted, and my arms are open wide, 'Cause you know just what to say, And you know just what to do, And I want to tell you so much, I love you.</p>
                 </div>
-            </b-card >
-            <b-card bg-variant="light" text-variant="black" class="text-center"  style="height: 161px">
-                <button>Sign up for our news letter</button>
+            </section >
+            <section class="newsLetter"  style="height: 161px">
                 <p>Get weekly plants care guide to prevent mental problems after loosing your green friends.</p>
-            </b-card>
+                <button>Sign up for our news letter</button>
+            </section>
 
             <b-card bg-variant="light" text-variant="black" class="text-center"  style="height: 250px">
                 <h2>Service Option</h2>
@@ -68,33 +102,47 @@
     export default {
         name: 'Flowers'
     }
+
 </script>
 
 <style>
     * {
         font-family: 'Montserrat', sans-serif;
     }
-        
-    nav .card-header {
-        height: 80px;
-        font-size: 200%;
+
+    body {
+        margin-top: 10px;
     }
         
+    .navbar-toggler {
+        width: 60px;
+    }
+
+    .title {
+        height: 780px;
+        background: url(./img/image-2.png) no-repeat;
+        background-size: cover;
+    }
+
     .heading {
+        font-size: 48px;
+        top: 452px;
+        left: 19px;
         width: 291px;
         height: 177px;
         color: #71B2FF;
         margin-top: 1em;
-        font-size: 38px;
         font-weight: bold;
         text-align: left;
         position: relative;
-        top: 320px;
-        left: 30px;
+    }
+
+    .card-text {
+        margin: -5px 0;
     }
 
     .service {
-        margin-top: -1.25em;
+        margin: -10px 0 50px 0;
     }
 
     .did {
@@ -105,7 +153,7 @@
 
     .care {
         text-align: left;
-        margin-right: 1.8em;
+        margin-right: 2.8em;
 
     }
     
@@ -118,6 +166,15 @@
     .story h2 {
         color: #449AFF;
         font-weight: bold;
+    }
+
+    .newsLetter {
+        background-color: #ECF5FF;
+    }
+
+    .newsLetter button {
+        width: 300px;
+        height: 50px;
     }
 
     .option ul {
